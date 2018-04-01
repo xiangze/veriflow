354fa2e76c6d784a5e2db29fad16e57e48d60c92
xiangze <apple@xiangze-no-MacBook-puro.local>
2018-04-01 18:46:09 +0900

    runnable

--- veriflow.py
+++ veriflow.py
@@ -1,23 +1,27 @@
 import os
 
-def __gendefs(name,sp,shape=[1],bits=8,issigned=False,end=";"):
-        if(self.dim==1):
-            return "%s [%d:0] %s%s"%(sp,shape[0],name,end)
-        elif(self.dim==2):
-            return "%s [%d:0][%d:0] %s%s"%(sp,shape[0],shape[1],name,end)
-        elif(self.dim==3):
-            return "%s [%d:0][%d:0][%d:0] %s%s"%(sp,shape[0],shape[1],shape[2],name,end)
+debug=False
+
+def _dprint(s):
+    if(debug):print(s)
+
+def _gendefs(name,sp,shape=[],bits=8,issigned=False,end=";"):
+    dim=len(shape)
+
+    if(dim==0):
+        return "%s [%d:0] %s%s"%(sp,bits-1,name,end)
+    elif(dim==1):
+        return "%s [%d:0][%d:0] %s%s"%(sp,bits-1,shape[0]-1,name,end)
+    elif(dim==2):
+        return "%s [%d:0][%d:0][%d:0] %s%s"%(sp,bits-1,shape[0]-1,shape[1]-1,name,end)
+    else:
+        return "%s [%d:0][%d:0][%d:0][%d:0] %s%s"%(sp,bits-1,shape[0]-1,shape[1]-1,shape[2]-1,name,end)
 
 class Variable(object):
-    def gendefs(self,name,sp,shape=[1],bits=8,issigned=False,end=";"):
-        if(self.dim==1):
-            return "%s [%d:0] %s%s"%(sp,shape[0],name,end)
-        elif(self.dim==2):
-            return "%s [%d:0][%d:0] %s%s"%(sp,shape[0],shape[1],name,end)
-        elif(self.dim==3):
-            return "%s [%d:0][%d:0][%d:0] %s%s"%(sp,shape[0],shape[1],shape[2],name,end)
+    def gendefs(self,name,sp,shape=[],bits=8,issigned=False,end=";"):
+        return _gendefs(name,sp,shape,bits,issigned,end)
 
-    def __init__(self,name,sp,shape=[1],bits=8,issigned=False,end=";"):
+    def __init__(self,name,sp,shape=[],bits=8,issigned=False,end=";"):
         self.dim=len(shape)
         self.name=name
         self.shape=shape
@@ -40,41 +44,41 @@ class Variable(object):
             l.dump()
 
 class Wire(Variable):        
-    def __init__(self,name,ops=None,shape=[1],bits=8,issigned=False):
+    def __init__(self,name,ops=None,shape=[],bits=8,issigned=False):
         super().__init__(name,"wire",shape,bits,issigned)
         if(ops!=None):
             self.s="assign %s=%s;"%(name,ops.s)
             self.leaf=ops["leaf"]
 
 class Reg(Variable):        
-    def __init__(self,name,conds_values,shape=[1],bits=8,issigned=False,latency=1):
+    def __init__(self,name,conds_values,shape=[],bits=8,issigned=False,latency=1):
         super().__init__(name,"reg",shape,bits,issigned)
 
-        self.s="always(posedge clock org negedge resetn)begin"
-        self.s+="%s <='0;"%name
+        self.s="always(posedge clock org negedge resetn)begin\n"
+        self.s+="%s <='0;\n"%name
         if(type(conds_values)==list):#[condition::s:value::{s:leaf} ]
             self.leaf=[]
             for conds_values in conds_values:
                 for k,v in conds_value.items():
-                    self.s+="end else if (%s)begin"%k
-                    self.s+="%s <=%s;"%(name,v.s)
+                    self.s+="end else if (%s)begin\n"%k
+                    self.s+="%s <=%s;\n"%(name,v.s)
                     self.leaf.appends(v)
         else:#op::{s:leaf}
-            self.s+="end else begin"
-            self.s+="%s <=%s;"%(name,conds_values["s"])
+            self.s+="end else begin\n"
+            self.s+="%s <=%s;\n"%(name,conds_values["s"])
             self.leaf=conds_values["leaf"]
-        self.s+="end"
+        self.s+="end\n"
 
 
 class Input(Variable):        
-    def __init__(self,name,shape=[1],bits=8,issigned=False):
+    def __init__(self,name,shape=[],bits=8,issigned=False):
         super().__init__(name,"input",shape,bits,issigned,"")
 
     def putreg(self):
         return _gendefs(self.name,"reg",self.shape,self.bits,self.issigned,";")
 
 class Output(Variable):        
-    def __init__(self,name,ops,shape=[1],bits=8,issigned=False):
+    def __init__(self,name,ops,shape=[],bits=8,issigned=False):
         super().__init__(name,"output",shape,bits,issigned,"")
         self.s="assign %s=%s;"%(name,ops.s)
         self.leaf=ops["leaf"]
@@ -95,32 +99,41 @@ def sub(l,r):
 def multiply(l,r):
     return op2("*",l,r)
 
-"""
-def _getinputs(t,ins=[]):
-    for l in t.leaf:
-        if(isinstance(l,Input)):
-            ins.append(l)
-        else:
-            ins=_getinputs(l,ins)
-    return ins
-"""
-def _getinputs(t,ins=[]):
+def _getinputs1(t,ins=[]):
     if(isinstance(t,Input)):
         ins.append(t)
+        _dprint("getinputs input")
+    elif(isinstance(t,list)):
+        _dprint("getinputs list")
+        _dprint(ins)
+        for l in t:
+            ins=_getinputs1(l,ins)
     else:
-        for l in t:#leaf
-            ins=_getinputs(l,ins)
+        _dprint("getinputs leaf")
+        ins=_getinputs1(t.leaf,ins)
+    return ins
+
+def _getinputs(t,ins=[]):
+    return list(set(_getinputs1(t,ins)))
+
+def _getoutputs1(t,ins=[]):
+    if(isinstance(t,Output)):
+        ins.append1(t)
+        _dprint("getoutputs output")
+    elif(isinstance(t,list)):
+        _dprint("getoutputs list")
+        _dprint(ins)
+        for l in t:
+            ins=_getoutputs1(l,ins)
+    else:
+        _dprint("getoutputs leaf")
+        ins=_getoutputs1(t.leaf,ins)
     return ins
 
 def _getoutputs(t,ins=[]):
-    if(isinstance(t,Output)):
-        ins.append(t)
-    else:
-        for l in t:#leaf
-            ins=_getinputs(l,ins)
-    return ins
+    return list(set(_getoutputs1(t,ins)))
 
-def __flatten(ls):
+def _flatten(ls):
     flist=[]
     lls=[ls]
     while(len(lls)>0):
@@ -138,6 +151,7 @@ class Session(object):
        self.reset="resetn"
 
        self.modname=modname
+       self.modnametr=modname.split(".")[0]
        self.vars=[]
        self.seq=""
        self.s=""
@@ -149,7 +163,7 @@ class Session(object):
        forever begin
        #5 %s = ~%s;
         end
-      end"""%(self.clock,self.clock,self.clock)
+      end\n"""%(self.clock,self.clock,self.clock)
 
    def _resetp(self):
        return """
@@ -157,50 +171,61 @@ class Session(object):
        %s = 0;
        #1
        %s = 1;
-       end"""%(self.reset,self.reset)
+       end\n"""%(self.reset,self.reset)
 
    def __mod(self,vars):
-       s="module ("
-       s+=",".join([i.s for i in _getinputs(vars)])
-       s+=","
-       s+=",".join([o.s for o in _getinputs(vars)])       
-       s+=");"
+       s="module %s(\n"%(self.modnametr)
+       ios=_getinputs(vars)
+       ios+=_getoutputs(vars)
+       s+=",\n".join([i.defs for i in ios])
+       s+=");\n"
 
-       for v in vars:
-           s+=v.defs                      
-       for v in vars:
-           s+=v.s           
-       s+="endmodule"
+       s+="\n".join([v.defs for v in vars])  
+       s+="\n"
+       s+="\n".join([v.s for v in vars])  
+       s+="\n"
+       s+="endmodule\n"
        return s
 
    def _inst(self,vars,suf=""):
        ios=_getinputs(vars)
        ios+=_getoutputs(vars)
-       s="%s %s_%s("%(self.modname,self.modname,suf)
-       s+=",".join([ ".%s (%s%s)"%(i,i,suf) for i in ios ])
-       s+=");"
+       s="%s %s_%s("%(self.modnametr,self.modnametr,suf)+"\n"
+       s+=".%s (%s),"%(self.clock,self.clock)+"\n"
+       s+=".%s (%s),"%(self.reset,self.reset)+"\n"
+       s+=",".join([ ".%s (%s%s)\n"%(i.name,i.name,suf) for i in ios ])
+       s+=");\n"
        return s
 
    def run(self,f,feed_dict,wait=1):
-       ins=_getinputs(f)
        self.vars.append(f)
+       ins=_getinputs([f])
+
+       _dprint("*run::")
+       _dprint("inputs::")
+       _dprint(ins)
+       _dprint("vars:")
+       _dprint(self.vars)
 
        for k,v in feed_dict.items():
            for i in ins:
                if(k==i.name):
                    self.seq+="%s =%s;\n"%(i.name,v)
-#                   self.s+="#1;\n"
-                   self.seq+="repeat(%d)@posedge(%s);\n"%(wait,clock)
+                   if(wait>0):
+                       self.seq+="repeat(%d)@posedge(%s);\n"%(wait,clock)
 
    def __enter__(self):
-       pass
+       return self
 
    def __exit__(self, exc_type, exc_value, traceback):
+       _dprint("*exit")
+       _dprint("vars:")
+       _dprint(self.vars)
+
 #module
        self.fp=open(self.modname,'w')
        self.fp.write(self.__mod(self.vars))
        self.fp.close()
-
 #test
        self.s="module test();\n"
        self.s+="reg %s,%s;\n"%(self.clock,self.reset)
@@ -224,7 +249,7 @@ class Session(object):
        self.s+="endmodule\n"
 
        self.fs=open(self.ifname,'w')
-       self.fs.write(s)
+       self.fs.write(self.s)
        self.fs.close()
 
        try:
@@ -236,19 +261,20 @@ class Session(object):
                os.system("cd obj_dir && make -j -f %s.mk %s && cd ../"%(ifname,ifname))
                os.system("./"+ifname)
            else:
-               print("not supported.")
+               print("not supported simulator.")
                raise Exception
        except:
                print("tool is not installed or doesnt work.")
 
 
 if __name__=='__main__':
+#   debug=True
    a=Input("a")
    b=Input("b")
    c=add(a,b)
    cr=Reg("cr",c)
-   cr.dump()
+#   cr.dump()
 
-   with Session() as s:
-       s.run(cr,{a:1,b:2})
+   with Session() as ses:
+       ses.run(cr,{a:1,b:2})
 
